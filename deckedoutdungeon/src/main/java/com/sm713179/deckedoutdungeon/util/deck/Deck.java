e309c95f24f9d74198da49d5572e79808b192601
Sam McClatchey <samteamender@gmail.com>
2022-10-09 11:39:43 -0500

    Created Deck class

---
+++ deckedoutdungeon/src/main/java/com/sm713179/deckedoutdungeon/util/deck/Deck.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sm713179.deckedoutdungeon.util.deck;
+
+import com.sm713179.deckedoutdungeon.util.card.*;
+import java.util.Collections;
+import java.util.Stack;
+
+/**
+ *
+ * @author Samuel McClatchey
+ */
+public class Deck {
+    Stack<Card> deck;
+
+    public Deck(Stack<Card> deck) {
+        this.deck = deck;
+    }
+
+    public Stack<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Stack<Card> deck) {
+        this.deck = deck;
+    }
+    
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+    
+    public Card draw() {
+        Card card = deck.pop();
+        return card;
+    }
+    
+    public void discard(Card card) {
+        deck.push(card);
+        shuffle();
+    } 
+}
