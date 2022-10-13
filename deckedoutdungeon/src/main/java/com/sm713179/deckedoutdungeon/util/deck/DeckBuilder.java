3b60e0438e343d419255609af285c2e4f6c5c1c2
Sam McClatchey <samteamender@gmail.com>
2022-10-12 22:05:45 -0500

    Reduced number of import statements inside DeckBuilder

--- deckedoutdungeon/src/main/java/com/sm713179/deckedoutdungeon/util/deck/DeckBuilder.java
+++ deckedoutdungeon/src/main/java/com/sm713179/deckedoutdungeon/util/deck/DeckBuilder.java
@@ -8,14 +8,8 @@ import com.sm713179.deckedoutdungeon.util.card.*;
 import java.io.File;
 import java.io.IOException;
 import static java.lang.Integer.parseInt;
-import java.util.Stack;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 /**
