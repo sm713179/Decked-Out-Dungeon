6582a796d288f8ac1ec2b5ea4b343954f8790471
Sam McClatchey <samteamender@gmail.com>
2022-11-15 20:47:22 -0600

    Started work on a txt parser

--- src/main/java/com/sm713179/deckedoutdungeon/util/parser/ScoreParser.java
+++ src/main/java/com/sm713179/deckedoutdungeon/util/parser/ScoreParser.java
@@ -4,10 +4,63 @@
  */
 package com.sm713179.deckedoutdungeon.util.parser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 /**
  *
  * @author Samuel McClatchey
  */
 public class ScoreParser {
     
+    public static int readScore() {
+        int score = 0;
+        
+        try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader("res/highScore.txt"));
+            String line = br.readLine();
+            
+            if (line == null) {
+                writeScore(0);
+            } else {
+                try {
+                    score = Integer.parseInt(line);
+                    
+                } catch (NumberFormatException e) {
+                    writeScore(0);
+                }
+            }
+            
+        } catch (IOException e) {
+           createFile();
+        }
+        return score;
+    }
+    
+    public static void writeScore(int score) {
+        try (Writer wr = new FileWriter("res/highScore.txt")) {
+            wr.write(score);
+            wr.close();
+            
+        } catch (IOException e) {
+            createFile();
+        }
+    }
+    
+    public static void createFile() {
+         File highScore = new File("res/highScore.txt");
+         
+         try {
+             highScore.createNewFile();
+             writeScore(0);
+             
+         } catch (IOException e) {
+             System.out.println("Failed to create highScore.txt");
+         }
+    }
 }
